8563d781d270361a34713f0c828a8f4ea658a13d
MUsamaAbid <m.usamaabid.ua@gmail.com>
2023-12-12 20:38:12 +0500

    Giving cards to each deck

--- YetCardGame/Assets/Scripts/DeckManager.cs
+++ YetCardGame/Assets/Scripts/DeckManager.cs
@@ -13,20 +13,21 @@ public enum Player
 public class DeckManager : MonoBehaviour
 {
     [SerializeField] Player playerNumber;
-    [SerializeField] RectTransform ParentRectTransform;
+
+    [SerializeField] RectTransform ParentInHandDeck;
+    [SerializeField] RectTransform ParentOnTableDeck;
 
     [SerializeField] List<Card> InHandDeck;
+    [SerializeField] List<Card> OnTableDeck;
 
     [SerializeField] int cardOverLapUnits = 200;
-
-    [SerializeField] GameObject[] InHandDeckPlaces;
 
     private void Start()
     {
         ArrangeCardsInHandDeck();
     }
 
-    public void AddCardToDeck(Card card)
+    public void AddCardInHandDeck(Card card)
     {
         InHandDeck.Add(card);
         ArrangeCardsInHandDeck();
@@ -34,48 +35,60 @@ public class DeckManager : MonoBehaviour
 
     void ArrangeCardsInHandDeck()
     {
+        foreach(var c in InHandDeck)
+        {
+            c.transform.parent = ParentInHandDeck;
+        }
+
         if (InHandDeck.Count != 0)
         {
             if (InHandDeck.Count == 1)
             {
                 Vector2 pos = new Vector2(0, 0);
+                //InHandDeck[0].transform.parent = ParentInHandDeck;
                 InHandDeck[0].SetTargetPosition(pos);
             }
             else if (InHandDeck.Count % 2 == 0)
             {
                 int middle = InHandDeck.Count / 2;
+                //InHandDeck[middle].transform.parent = ParentInHandDeck;
+
                 Vector2 middlePos = new Vector2(cardOverLapUnits / 2, 0);
                 InHandDeck[middle].SetTargetPosition(middlePos);
 
                 for (int i = middle - 1; i >= 0; i--) //From 0 to middle is the right side of the deck
                 {
-                    //InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+                    //InHandDeck[i].transform.parent = ParentInHandDeck;
                     Vector2 pos = new Vector2((-(middle - i) * cardOverLapUnits) - (-cardOverLapUnits / 2), 0);
                     InHandDeck[i].SetTargetPosition(pos);
                 }
                 for (int i = InHandDeck.Count - 1; i > middle; i--) //From middle to up is the left side of the deck
                 {
+                    //InHandDeck[i].transform.parent = ParentInHandDeck;
                     Vector2 pos = new Vector2((((i + 1) - middle) * cardOverLapUnits) - (cardOverLapUnits / 2), 0);
-                    //   InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
                     InHandDeck[i].SetTargetPosition(pos);
                 }
             }
             else
             {
                 int middle = InHandDeck.Count / 2;
+                //InHandDeck[middle].transform.parent = ParentInHandDeck;
+
                 Vector2 middlePos = new Vector2(0, 0);
                 InHandDeck[middle].SetTargetPosition(middlePos);
 
                 for (int i = middle - 1; i >= 0; i--)
                 {
+                    //InHandDeck[i].transform.parent = ParentInHandDeck;
                     Vector2 pos = new Vector2(-(middle - i) * cardOverLapUnits, 0);
-                 //   InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+                    //   InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
                     InHandDeck[i].SetTargetPosition(pos);
                 }
                 for (int i = InHandDeck.Count - 1; i > middle; i--)
                 {
+                    //InHandDeck[i].transform.parent = ParentInHandDeck;
                     Vector2 pos = new Vector2((i - middle) * cardOverLapUnits, 0);
-                 //   InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
+                    //   InHandDeck[i].gameObject.GetComponent<RectTransform>().anchoredPosition = pos;
                     InHandDeck[i].SetTargetPosition(pos);
                 }
             }
--- YetCardGame/Assets/Scripts/GameplayCardManager.cs
+++ YetCardGame/Assets/Scripts/GameplayCardManager.cs
@@ -1,13 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class GameplayCardManager : MonoBehaviour
 {
-    [SerializeField] Card[] MainCardDeck;
-    [SerializeField] Card[] Player1Deck;
-    [SerializeField] Card[] Player2Deck;
+    [SerializeField] GameObject MainDeckParent;
+
+    [SerializeField] List<Card> MainCardDeck;
+    [SerializeField] List<Card> Player1Deck;
+    [SerializeField] List<Card> Player2Deck;
 
     [SerializeField] DeckManager Player1;
     [SerializeField] DeckManager Player2;
+
+    private void Start()
+    {
+        AddCardsToMainDeck();
+        ShuffleCards();
+        DistributeCards(Player.Player1, 1);
+        DistributeCards(Player.Player2, 1);
+    }
+    void AddCardsToMainDeck()
+    {
+        foreach (var cards in MainCardDeck)
+        {
+            cards.gameObject.transform.parent = MainDeckParent.transform;
+        }
+    }
+    private void ShuffleCards()
+    {
+        int cardCount = MainCardDeck.Count;
+
+        // Iterate through the list in reverse order
+        for (int i = cardCount - 1; i > 0; i--)
+        {
+            // Generate a random index within the remaining unshuffled cards
+            int randomIndex = UnityEngine.Random.Range(0, i + 1);
+
+            // Swap the current card with the randomly selected card
+            Card temp = MainCardDeck[i];
+            MainCardDeck[i] = MainCardDeck[randomIndex];
+            MainCardDeck[randomIndex] = temp;
+        }
+
+        // Now, the 'cards' list is shuffled
+    }
+    void DistributeCards(Player player, int cardAmount)
+    {
+        //Shuffle cards by swapping there places.
+        //Pick a random number and distribute from there
+        int r = UnityEngine.Random.Range(0, MainCardDeck.Count - cardAmount - 1);
+        for (int i = r; i < (r + cardAmount); i++)
+        {
+            if(player == Player.Player1)
+            {
+                Player1.AddCardInHandDeck(MainCardDeck[i]);
+                MainCardDeck.RemoveAt(i);
+            }
+            else if (player == Player.Player2)
+            {
+                Player2.AddCardInHandDeck(MainCardDeck[i]);
+                MainCardDeck.RemoveAt(i);
+            }
+        }
+
+    }
+    public void AddToPlayer1Deck(Card card)
+    {
+        Player1Deck.Add(card);
+        Player1.AddCardInHandDeck(card);
+    }
 }
